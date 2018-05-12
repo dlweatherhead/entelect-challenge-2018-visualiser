2ff15d44ca1833f2e2fed5b55a41162a612109da
David W <dlweatherhead@gmail.com>
2018-05-12 17:24:21 +0200

    Created test scene with GameManager to load example bot and state

---
+++ Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using EC2018.Entities;
+using Newtonsoft.Json;
+using EC2018;
+
+public class GameManager : MonoBehaviour {
+
+	private string exampleBot = "example-bot";
+	private string exampleState = "example-state";
+
+	void Start () {
+		LoadBotFromFile();
+		LoadStateFromFile();
+	}
+
+	void Update () {
+		
+	}
+
+	private void LoadBotFromFile() {
+		TextAsset botFile = Resources.Load<TextAsset> (exampleBot);
+		var bot = JsonConvert.DeserializeObject<Bot> (botFile.text);
+
+		Debug.Log (bot.nickName);
+	}
+
+	private void LoadStateFromFile() {
+		TextAsset stateFile = Resources.Load<TextAsset> (exampleState);
+
+		var gameState = JsonConvert.DeserializeObject<GameState>(stateFile.text);
+
+		Debug.Log (gameState.Players[0].PlayerType);
+	}
+}
