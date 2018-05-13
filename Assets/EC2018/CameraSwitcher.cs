dd9c35adbf316247b7c6ea12fd6dee092f12b4c5
David W <dlweatherhead@gmail.com>
2018-05-13 20:32:00 +0200

    Added Top Camera and switching with KeyCode.C

---
+++ Assets/EC2018/CameraSwitcher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EC2018 {
+
+	public class CameraSwitcher : MonoBehaviour {
+
+		public GameObject SideViewCamera;
+		public GameObject TopViewCamera;
+
+		private GameObject activeCamera;
+
+		void Start() {
+			SideViewCamera.SetActive(true);
+			TopViewCamera.SetActive(false);
+			activeCamera = SideViewCamera;
+		}
+
+		void Update () {
+			if (Input.GetKeyDown(KeyCode.C)) {
+				Debug.Log ("Switching Camera");
+				OnCameraSwithClicked ();
+			}
+		}
+
+		public void OnCameraSwithClicked() {
+			if(activeCamera == SideViewCamera) {
+				Debug.Log ("Top Camera Active");
+				TopViewCamera.SetActive(true);
+				SideViewCamera.SetActive(false);
+				activeCamera = TopViewCamera;
+			} else if (activeCamera == TopViewCamera) {
+				Debug.Log ("Side Camera Active");
+				SideViewCamera.SetActive(true);
+				TopViewCamera.SetActive(false);
+				activeCamera = SideViewCamera;
+			}
+		}
+	}
+}
