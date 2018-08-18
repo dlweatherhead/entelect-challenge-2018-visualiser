9aed9c9163161809b9634dae49e8ac5b04068f84
David W <dlweatherhead@gmail.com>
2018-08-18 15:54:24 +0200

    Updated batch script

--- run-replay.bat
+++
@@ -1,1 +0,0 @@
-entelect-visualiser.exe -round-step "0.1" -screen-fullscreen 1 -screen-width 1920 -screen-height 1080 -screen-quality Ultra
---
+++ start-visualiser.bat
@@ -0,0 +1,1 @@
+entelect-visualiser.exe -round-step "1" -screen-fullscreen 1 -screen-width 1600 -screen-height 900 -screen-quality Ultra
