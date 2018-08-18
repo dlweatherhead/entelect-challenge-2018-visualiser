c8b3fad7de44ff7d161be7a01aa2b43e467badee
David W <dlweatherhead@gmail.com>
2018-08-18 15:50:48 +0200

    Populate player names in Count In screen

---
+++ Assets/CountInDetailPopulator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using EC2018.Enums;
+using UnityEngine.UI;
+using EC2018;
+
+public class CountInDetailPopulator : MonoBehaviour {
+
+	public Text namePlayerA;
+	public Text namePlayerB;
+
+	void Start () {
+		namePlayerA.text = GetPlayerName (PlayerType.A);
+		namePlayerB.text = GetPlayerName (PlayerType.B);
+	}
+
+	public string GetPlayerName(PlayerType playerType) {
+		var allPlayers = Directory.GetDirectories(GetReplayPathFromPrefs() + "/Round 000");
+		if (playerType == PlayerType.A) {
+			return new DirectoryInfo(allPlayers[0]).Name;
+		}
+		return new DirectoryInfo(allPlayers[1]).Name;
+	}
+
+	string GetReplayPathFromPrefs() {
+		return PlayerPrefs.GetString(Constants.PlayerPrefKeys.SelectedReplay);
+	}
+}
